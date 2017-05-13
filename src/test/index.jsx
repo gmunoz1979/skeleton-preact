b4adc49e6d402bba0a4127ef84f5fa6eef2d6606
Gerard Muñoz <gmunoz1979@gmail.com>
2017-05-13 18:40:25 -0300

    Se añade estructura para trabajar con preact

---
+++ src/test/index.jsx
@@ -0,0 +1,7 @@
+import {h, render} from 'preact';
+import '../sass/default.scss';
+
+render(
+  <div className='skeleton'>
+    <h1>Skeleton preact</h1>
+  </div>, document.body);
---
+++ webpack.config.js
@@ -0,0 +1,107 @@
+const path              = require('path');
+const webpack           = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+require('babel-polyfill');
+
+const plugins = [];
+
+const NODE_ENV      = process.env.NODE_ENV;
+const ISPRODUCTION  = NODE_ENV === 'production';
+const ISDEVELOPMENT = NODE_ENV === 'development';
+
+if (ISDEVELOPMENT) {
+  plugins.push(new HtmlWebpackPlugin({
+    template: path.resolve('src', 'templates', 'default.html')
+  }));
+
+  const browserSync = {};
+
+  browserSync.host = 'localhost';
+  browserSync.port = 3000;
+  browserSync.server = { baseDir: ['www'] };
+
+  plugins.push(new webpack.NoEmitOnErrorsPlugin());
+  plugins.push(new BrowserSyncPlugin(browserSync));
+}
+
+plugins.push(new ExtractTextPlugin('css/[name].css'));
+
+ISPRODUCTION &&
+  plugins.push(new webpack.optimize.UglifyJsPlugin({
+    compress: {
+      warnings: false,
+      screw_ie8: true,
+      conditionals: true,
+      unused: true,
+      comparisons: true,
+      sequences: true,
+      dead_code: true,
+      evaluate: true,
+      if_return: true,
+      join_vars: true,
+    },
+    output: {
+      comments: false,
+    },
+  }));
+
+const file_entry  = ISPRODUCTION ? 'js/index.js' : 'test/index.jsx';
+const path_output = ISPRODUCTION ? './dist' : './www';
+
+const entry = [];
+ISDEVELOPMENT && entry.push('babel-polyfill');
+entry.push(path.resolve(__dirname, 'src', file_entry));
+
+const config = {
+  entry: entry,
+  output: {
+    path: path.resolve(__dirname, path_output),
+    publicPath: '/',
+    filename: 'js/skeleton-preact.js',
+    sourceMapFilename: '[file].map',
+  },
+  devtool: ISPRODUCTION ? 'eval' : 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        enforce: 'pre',
+        use: 'source-map-loader'
+      },
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: [
+          'babel-loader',
+          'eslint-loader',
+        ]
+      },
+      {
+        test: /\.(scss|css)$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: [
+            'css-loader?sourceMap',
+            'sass-loader?sourceMap'
+          ]
+        })
+      },
+      {
+        test: /\.(woff2?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'file-loader?limit=1024&name=./fonts/[name].[ext]'
+      },
+      {
+        test: /\.(jpe?g|png)$/,
+        loader: 'file-loader?limit=1024&name=./images/[name].[ext]'
+      }
+    ]
+  },
+  plugins: plugins,
+  resolve: {
+    extensions: ['.js', '.jsx']
+  }
+};
+
+module.exports = config;
